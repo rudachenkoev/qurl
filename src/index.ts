4b026f5245563d15b8f77436c3c672a8dd0ae34c
rudachenkoev <rudachenkoevgeniy@gmail.com>
2024-01-05 14:23:05 +0100

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import 'dotenv/config'
 import http from 'http'
+import cors from 'cors'
 import { json } from 'body-parser'
 import feedBackRoutes from '@routes/feedback'
 import authRoutes from '@routes/auth'
@@ -9,6 +10,7 @@ import { setupDatabaseConnection } from '@config/db'
 const app = express()
 
 app.use(json())
+app.use(cors())
 app.use('/api/v1/feedbacks', feedBackRoutes)
 app.use('/api/v1/auth', authRoutes)
 
