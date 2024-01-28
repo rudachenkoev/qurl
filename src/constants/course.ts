46a1c0e1379ba0aaa91cc20d4e30283db7c29506
rudachenkoev <rudachenkoevgeniy@gmail.com>
2024-01-28 22:28:20 +0100

    temporary schema init

---
+++ src/constants/course.ts
@@ -0,0 +1,11 @@
+export const promotionPeriods = [
+  { id: 1, name: '1 day' },
+  { id: 2, name: '5 days' },
+  { id: 3, name: '15 days' },
+  { id: 4, name: '1 month' }
+]
+
+export const tags = [
+  { id: 'top', description: 'Paid promotion' },
+  { id: 'gaining_popularity', description: 'Gaining popularity. Set automatically when course sales go sharply upwards' },
+]
---
+++ src/models/course.ts
@@ -0,0 +1,73 @@
+import { Schema, model } from 'mongoose'
+import { promotionPeriods, tags } from '@/constants/course'
+type ICourse = {
+}
+
+
+const CourseSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    default: ''
+  },
+  courseCategory: {
+    type: Schema.Types.ObjectId,
+    ref: 'directories_CourseCategory',
+    required: true
+  },
+  poster: {
+    type: String,
+    default: ''
+  },
+  timesSold: {
+    type: Number,
+    default: 0
+  },
+  tag: {
+    default: null,
+    validate: {
+      validator: function(value: string) {
+        if (value) return tags.some(period => period.id === value)
+        else return value === null
+      },
+      message: 'Invalid tag'
+    }
+  },
+  price: {
+    type: Number,
+    set: function (value: number) { return value.toFixed(2) },
+    required: true
+  },
+  promotionPeriod: {
+    default: null,
+    validate: {
+      validator: function(value: number) {
+        if (value) return promotionPeriods.some(period => period.id === value)
+        else return value === null
+      },
+      message: 'Invalid promotion period'
+    }
+  },
+  promotionValidTo: {
+    type: Date,
+    default: null
+  },
+  language: {
+    type: String,
+    enum: ['en', 'de', 'fr', 'es', 'it'],
+    required: true
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, { timestamps: true })
+
+export const CourseModel = model('Course', CourseSchema)
+export const getCourses = () => CourseModel.find()
+// export const getCourseById = (id: string) => CourseModel.findById(id)
+export const createCourse = (attr: ICourse) => new CourseModel(attr).save().then(item => item.toObject())
